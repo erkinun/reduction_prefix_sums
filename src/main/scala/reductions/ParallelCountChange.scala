64d79b9a02cf45bd23bb877090f72b1f98c82d59
Erkin Unlu <eunlu@tripadvisor.com>
2016-09-16 11:29:33 +0100

    implemented the older seq algorithm

--- src/main/scala/reductions/ParallelCountChange.scala
+++ src/main/scala/reductions/ParallelCountChange.scala
@@ -48,8 +48,9 @@ object ParallelCountChange {
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money < 0) 0
     else if (money == 0) 1
+    else if (coins.isEmpty) 0
     else {
-      ???
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
     }
   }
 
