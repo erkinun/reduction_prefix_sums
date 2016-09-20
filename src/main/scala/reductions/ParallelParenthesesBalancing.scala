776e1e5d82d24341c857e4e939802e0e466ca6e6
Erkin Unlu <eunlu@tripadvisor.com>
2016-09-20 09:47:57 +0100

    removed the inner function that converts the array to list which was taking for ages

--- src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -21,6 +21,7 @@ object ParallelParenthesesBalancingRunner {
     val length = 100000000
     val chars = new Array[Char](length)
     val threshold = 10000
+    println("before starting the warmup")
     val seqtime = standardConfig measure {
       seqResult = ParallelParenthesesBalancing.balance(chars)
     }
@@ -42,33 +43,51 @@ object ParallelParenthesesBalancing {
    */
   def balance(chars: Array[Char]): Boolean = {
 
-    def innerBalance(chars: List[Char], remLeft: Int): Boolean = chars.toList match {
-      case Nil => remLeft == 0
-      case c :: cs =>
-        if (c == '(') innerBalance(cs, remLeft + 1)
+    def innerBalanceWithArray(idx:Int, remLeft: Int): Boolean = {
+      if (idx < chars.length) {
+        val c = chars(idx)
+        if (c == '(') innerBalanceWithArray(idx + 1, remLeft + 1)
         else if (c == ')') {
           if (remLeft == 0) false
-          else innerBalance(cs, remLeft - 1)
+          else innerBalanceWithArray(idx + 1, remLeft - 1)
         }
-        else innerBalance(cs, remLeft)
+        else innerBalanceWithArray(idx + 1, remLeft)
+      }
+      else remLeft == 0
     }
 
-    innerBalance(chars.toList, 0)
+    //innerBalance(chars.toList, 0)
+    innerBalanceWithArray(0, 0)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
+    def traverse(idx: Int, until: Int, leftPar: Int, rightPar: Int): Int = {
+//      if (idx < until) {
+//        val charIdx = chars(idx)
+//        if (charIdx == '(') traverse(idx + 1, until, leftPar + 1, rightPar)
+//        else if (charIdx == ')') traverse(idx + 1, until, leftPar, rightPar + 1)
+//        else traverse(idx + 1, until, leftPar, rightPar)
+//      }
+//      else leftPar + rightPar
       ???
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
+    def reduce(from: Int, until: Int): Int = {
+//      if (until - from < threshold) {
+//        traverse(from, until, 0, 0)
+//      }
+//      else {
+//        val mid = from + ((until - from) / 2)
+//        val balances = parallel(reduce(from, mid), reduce(mid, until))
+//        balances._1 + balances._2
+//      }
       ???
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == 0
   }
 
   // For those who want more:
